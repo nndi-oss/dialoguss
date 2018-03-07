e5f82876c30337424b5fa76cb402f0bc8396f431
Zikani Nyirenda Mwase <zikani.nmwase@ymail.com>
2018-03-07 05:31:40 +0200

    Fix matching issues in mock_server
    Made the mock server as simplistic as possible on the matching of incoming text, might use sqlite later

--- dialoguss/mock_server.py
+++ dialoguss/mock_server.py
@@ -1,18 +1,37 @@
-from flask import Flask, request, g
+"""This is a simple mock ussd serve. DO NOT use this as an example of 
+what an actual USSD request handling server should be implemented, please.
+"""
+import os
+import re
+from flask import Flask, request, session
 
 test_app = Flask(__name__)
 test_app.debug = True
+test_app.secret_key = os.urandom(5)
 
 @test_app.route("/ussd", methods = [ 'POST' ])
 def test_ussd_handler():
     """Test handler"""
     session_id = request.form['sessionId']
-    
     text = request.form['text']
+    test_app.logger.debug('Got text: {}'.format(text))
 
-    if text == "":
+    if len(text) < 1:
         return """What is your name?
 CONTINUE"""
+
+    
+    if re.match("name\:", text) is not None:
+        name = text.replace("name:", "").strip()
+        res_ = """Welcome, {}
+Choose an item:
+1. Account detail
+2. Balance
+3. Something else
+# Exit
+CONTINUE
+""".format(name)
+        return res_
 
     if text == "1" or text == "1.":
         return """Your account is inactive
@@ -28,7 +47,6 @@ CONTINUE
         return """There is nothing else, /(^_^)\\
 END
 """
-
     return "END"
 
 if __name__ == '__main__':
